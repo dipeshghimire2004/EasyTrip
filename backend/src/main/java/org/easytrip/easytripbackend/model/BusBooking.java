12a3853885a37fab3923b3bad5062825acff9017
dipeshghimire2004 <ghimiredipesh2004@gmail.com>
2025-05-11 15:43:50 +0545

    resolve:guesthouse register issue

--- backend/src/main/java/org/easytrip/easytripbackend/model/BusBooking.java
+++ backend/src/main/java/org/easytrip/easytripbackend/model/BusBooking.java
@@ -1,29 +1,29 @@
-package org.easytrip.easytripbackend.model;
-import jakarta.persistence.*;
-import lombok.Data;
-import java.time.LocalDateTime;
+    package org.easytrip.easytripbackend.model;
+    import jakarta.persistence.*;
+    import lombok.Data;
+    import java.time.LocalDateTime;
 
-@Entity
-@Data
-public class BusBooking {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Entity
+    @Data
+    public class BusBooking {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id", nullable = false)
-    private User client;
+        @ManyToOne
+        @JoinColumn(name = "client_id", nullable = false)
+        private User client;
 
-    @ManyToOne
-    @JoinColumn(name = "bus_id", nullable = false)
-    private Bus bus;
+        @ManyToOne
+        @JoinColumn(name = "bus_id", nullable = false)
+        private Bus bus;
 
-    @Column(nullable = false)
-    private LocalDateTime bookingTime;
+        @Column(nullable = false)
+        private LocalDateTime bookingTime;
 
-    @Column(nullable = false)
-    private int seatsBooked;
+        @Column(nullable = false)
+        private int seatsBooked;
 
-    @Column(nullable = false)
-    private boolean isCancelled = false;
-}
+        @Column(nullable = false)
+        private boolean isCancelled = false;
+    }
